24fc4924f2e1b4afce6c29b32697bfc2981675a0
Messitos16 <149245706+Messitos16@users.noreply.github.com>
2024-01-17 11:52:17 +0300

    Add files via upload

---
+++ 3/script.js
@@ -0,0 +1,213 @@
+const Muhammeddior = {
+    name: 'Muhammeddior',
+    age: 16,
+    region: 'Osh',
+    address: {
+        street: 'Djim',
+        number: '174',
+        region: 'Osh',
+    },
+    freands: {
+        name: 'Bael',
+        age: '15',
+        region: 'Osh',
+        address: {
+            street: 'Roompea',
+            number: '124',
+            region: 'Everton',
+        },
+        freands2: {
+            name: 'Kolya',
+            age: '15',
+
+            address: {
+                street: 'Caragar',
+                number: '111',
+                region: 'Osh',
+            },
+        },
+        freands3: {
+            name: 'Berador',
+            age: '15',
+            region: 'Osh',
+            address: {
+                street: 'Lonspay',
+                number: '126',
+                region: 'Osh',
+            },
+        },
+        freands4: {
+            name: 'Nurseit',
+            age: '18',
+            region: 'Osh',
+            address: {
+                street: 'Kolstreet',
+                number: '12',
+                region: 'Osh',
+            },
+        },
+        freands5: {
+            name:'Ilon-Musk',
+            age:43,
+            address: {
+                street: 'Bromstreet',
+                number: '14',
+                region: 'Texas',
+            },
+        },
+        freands6: {
+            name:'Messi',
+            age:36,
+            address: {
+                street: 'Messitoss',
+                number: '133',
+                region: 'Argentina',
+            },
+        },
+        freands7: {
+            name:'Cristiano',
+            age:39,
+            address: {
+                street: 'Kolstreet',
+                number: '12',
+                region: 'Osh',
+            },
+        }
+    },
+    Hobbies: [
+
+         'Football', 'Basketball',
+        {gamename: ['Efootball', 'Fc24']}
+    ],
+    
+    Education: [
+        'Gramotey',
+        'Kirova',
+        {
+            category: 'high',
+        }
+    ],
+    ThingsILiketodo: [
+        'Eating',
+        'Scroling TikTok',
+        'watching matches',
+        'playing Football',
+        'playing online games',
+        'watching movies',
+    ],
+LanguagesIknow:[
+    'English',
+    'Russhian',
+    'Kyrgyz',
+    'Uzbek',
+    'Turkish'
+],
+LanguagesIwantTolearn:[
+'Arabic',
+'Spanish',
+'Korean',
+'Italian',
+],
+FavotiteFoods:[
+    'Manty',
+    'Samsy',
+    'Shashlyk',
+    'Sushi',
+    'Ramen',
+],
+FavoriteScooleSubjects:[
+    'English',
+    'Turkish',
+    'Biology',
+    'Chemistry',
+    'Informatics',
+    'PE'
+],
+SubjectsIdontLike:[
+    'Physics',
+    'History',
+    'literature',
+    'drawing'
+    
+],
+FavoriteCars:[
+    'Tesla',
+    'LI9',
+    'Mercedes',
+    'BMW I8'
+],
+countiesIwouldLiketoVisit: [
+    'Netherlands',
+    'Barcelona',
+    'South-Korea',
+    'Britain',
+    'Miami'
+],
+YoutubeChannelsIwatch: [
+'Fc-Barcelona',
+'Barca-Family',
+'Sky-Sports',
+'Dima-Maslennikov',
+],
+ClotheStyleIlike:[
+    'Old-money',
+    'Starboy',
+    'Streatwear',
+],
+ClubsIlike:[
+    'Fc-Barcelona',
+    'Liverpool',
+    'Bayern-Munich',
+    'Milan'
+],
+ClubsIhate:[
+    'Real-Madrid',
+    'Manchester-city',
+    'Manchester-United',
+    'Inter-Milan'
+],
+AnimalsIlike:[
+    'Horse',
+    'Cat',
+    ''
+],
+MyFavoriteFotballer:[
+    'Messi',
+    'Cristiano',
+    'Xavi',
+    'Inesta',
+    'Ozil',
+
+],
+FootballerPlaystyleIlike:[
+    'Xavi',
+    'Inesta',
+    'Messi'
+],
+Family:{
+    Sisters:2,
+    Brothers:2,
+    Mother:1,
+    Father:1
+},
+ForeingPeopleIwanttoTalk:{
+    Answer:[
+        'Spanish',
+        'Turkish',
+        'Native-American',
+        'Korean',
+    ]
+},
+StadiumIlikeTobe:[
+    'Camp-Nou',
+    'Anfield',
+],
+ThingsIliketoDo2:[
+'PLaing-Soccer',
+'Plaing video games',
+'Watching films',
+'Codding', 
+],
+}
+
+console.log(Muhammeddior) 
